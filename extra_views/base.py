8668d46b4a2f55d4caae2924914e2390ec07d665
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2011-05-18 18:38:08 +0100

    more work on formset view

--- extra_views/base.py
+++ extra_views/base.py
@@ -1,19 +1,28 @@
 from django.views.generic.base import TemplateResponseMixin, View
 from django.http import HttpResponseRedirect
+from django.forms.formsets import formset_factory
 
 class FormsetMixin(object):
     initial = {}
-    formset_class = None
+    form_class = None
+    formset_class = None  # Sometimes used if we want validation on the overall formset
     success_url = None
+    extra = 2
+    max_num = 1
+    can_order = False
+    can_delete = False
 
     def get_initial(self):
-      return self.initial
+        return self.initial
 
     def get_formset_class(self):
         return self.formset_class
 
+    def get_form_class(self):
+        return self.form_class
+
     def get_formset(self):
-        pass
+        return formset_factory(self.get_form_class(), formset=self.get_formset_class(), extra=self.extra, max_num=self.max_num, can_order=self.can_order, can_delete=self.can_delete)
 
     def get_formset_kwargs(self):
         kwargs = {'initial': self.get_initial()}
@@ -28,6 +37,7 @@ class FormsetMixin(object):
         else:
             # Default to returning to the same page
             url = self.request.get_full_path() 
+        return url
 
     def formset_valid(self, formset):
         return HttpResponseRedirect(self.get_success_url())
@@ -41,13 +51,11 @@ class ProcessFormsetView(View):
     A mixin that processes a fomset on POST.
     """
     def get(self, request, *args, **kwargs):
-        formset_class = self.get_formset_class()
-        formset = self.get_form(formset_class)
+        formset = self.get_formset()(initial=self.get_initial())
         return self.render_to_response(self.get_context_data(formset=formset))
 
     def post(self, request, *args, **kwargs):
-        formset_class = self.get_formset_class()
-        formset = self.get_formset(formset_class)
+        formset = self.get_formset()(request.POST, request.FILES, initial=self.get_initial())
         if formset.is_valid():
             return self.formset_valid(formset)
         else:
@@ -56,12 +64,14 @@ class ProcessFormsetView(View):
     def put(self, *args, **kwargs):
         return self.post(*args, **kwargs)
 
+
 class BaseFormsetView(FormsetMixin, ProcessFormsetView):
     """
     A base view for displaying a formset
     """
 
-class FormsetView(TemplateResponseMixin, BaseFormView):
+
+class FormsetView(TemplateResponseMixin, BaseFormsetView):
     """
     A view for displaying a form, and rendering a template response
     """
