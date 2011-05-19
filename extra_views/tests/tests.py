642a4704feb55c33ae1d016d07b3d33ad8cbcd80
Andrew Ingram <andy@andrewingram.net>
2011-05-20 00:55:27 +0100

    added create test for FormsetView

--- extra_views/tests/tests.py
+++ extra_views/tests/tests.py
@@ -3,8 +3,11 @@ from django.test import TestCase
 class FormsetTests(TestCase):
     urls = 'extra_views.tests.urls'
     
-    def test_one(self):
+    def test_create(self):
         res = self.client.get('/formset/simple/')
         self.assertEqual(res.status_code, 200)
-        
-        print res 
+        self.assertTrue('formset' in res.context)
+        self.assertFalse('form' in res.context)
+        self.assertTemplateUsed(res, 'extra_views/address_formset.html')
+        self.assertEquals(res.context['formset'].__class__.__name__, 'AddressFormFormSet')
+
