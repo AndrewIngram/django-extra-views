d9dd1f274713033fe8b644106d43a305aa8932d4
Andrew Ingram <andy@andrewingram.net>
2011-06-12 19:05:23 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='django-extra-view',
+      version='0.1.0',
+      url='https://github.com/AndrewIngram/django-extra-views',
+      description="Extra class-based views for Django",
+      author="Andrew Ingram",
+      author_email="andy@andrewingram.net",
+      package_dir={'': '.'},
+)
