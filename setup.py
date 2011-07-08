855f104fc47c572821876fd995d548a86bc936ed
AndrewIngram <andy@andrewingram.net>
2011-07-08 01:57:13 -0700

    Edited setup.py via GitHub

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
-setup(name='django-extra-views',
-      version='0.1.0',
-      url='https://github.com/AndrewIngram/django-extra-views',
-      description="Extra class-based views for Django",
-      author="Andrew Ingram",
-      author_email="andy@andrewingram.net",
-      package_dir={'': '.'},
+setup(
+    name='django-extra-views',
+    version='0.1.1',
+    url='https://github.com/AndrewIngram/django-extra-views',
+    description="Extra class-based views for Django",
+    long_description=open('README.rst').read(),
+    author="Andrew Ingram",
+    author_email="andy@andrewingram.net",
+    package_dir={'': '.'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',   
+        'Programming Language :: Python']
 )
