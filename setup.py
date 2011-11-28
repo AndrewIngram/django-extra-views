29edaaecacc2cb013929be6a121f48188daf6d41
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2011-11-28 12:42:28 +0000

    bumped version to 0.2.0 and added license to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='django-extra-views',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/AndrewIngram/django-extra-views',
     description="Extra class-based views for Django",
     long_description=open('README.rst').read(),
+    license="MIT",
     author="Andrew Ingram",
     author_email="andy@andrewingram.net",
     packages=['extra_views'],
