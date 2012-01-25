c0a933aba5cb73d0d03da00c5c5ddf19aeca2339
Andrew Ingram <andrew.ingram@tangentlabs.co.uk>
2012-01-25 14:56:50 +0000

    Added is_current_month to calendar dictionaries, and raise an exception if someone tries to use pagination

--- extra_views/dates.py
+++ extra_views/dates.py
@@ -1,17 +1,28 @@
 from django.views.generic.dates import BaseMonthArchiveView
 from django.views.generic.list import MultipleObjectTemplateResponseMixin
+from django.core.exceptions import ImproperlyConfigured
 from calendar import Calendar
 from collections import defaultdict
 import datetime
 
+
 class BaseCalendarMonthArchiveView(BaseMonthArchiveView):
     first_of_week = 0  # 0 = Monday, 6 = Sunday
+    paginate_by = None
+
+    def get_paginate_by(self, queryset):
+        if self.paginate_by is not None:
+            raise ImproperlyConfigured(u"'%s' cannot be paginated, it is a calendar view" % self.__class__.__name__)
+        return None
+        
+    def get_first_of_week(self):
+        return self.first_of_week
     
     def get_context_data(self, **kwargs):
         data = super(BaseCalendarMonthArchiveView, self).get_context_data(**kwargs)
         date = data['date_list'][0]
         
-        cal = Calendar(self.first_of_week)
+        cal = Calendar(self.get_first_of_week())
 
         month_calendar = []
         now = datetime.datetime.utcnow()
@@ -34,6 +45,7 @@ class BaseCalendarMonthArchiveView(BaseMonthArchiveView):
                     'day': day,
                     'object_list': date_lists[day],
                     'today': day == now.date(),
+                    'is_current_month': day.month == date.month,
                 })
             month_calendar.append(week_calendar)
             
