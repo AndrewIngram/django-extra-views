61d26cce1a79befe3073051f57673cbb5a178a0c
Tom Christie <tom@tomchristie.com>
2013-09-26 09:29:29 +0100

    Factoring away classes

--- extra_views/formsets.py
+++ extra_views/formsets.py
@@ -158,33 +158,28 @@ class ModelFormSetView(GenericModelFormSetView):
 
 ######## oldskool #########
 
-class BaseFormSetMixin(object):
-    """
-    Base class for constructing a FormSet within a view
-    """
 
+class BaseInlineFormSetMixin(object):
+    """
+    Base class for constructing an inline formSet within a view
+
+    IMPORTANT: Because of a Django bug, initial data doesn't work here in Django 1.3
+    """
+    model = None
+    inline_model = None
+    success_url = None
+    fk_name = None
     initial = []
     form_class = None
-    formset_class = None
-    success_url = None
+    formset_class = BaseInlineFormSet
+    exclude = None
+    fields = None
+    formfield_callback = None
     extra = 2
     max_num = None
     can_order = False
-    can_delete = False
-
-    def construct_formset(self):
-        """
-        Returns an instance of the formset
-        """
-        formset_class = self.get_formset()
-        extra_form_kwargs = self.get_extra_form_kwargs()
-
-        # Hack to let as pass additional kwargs to each forms constructor. Be aware that this
-        # doesn't let us provide *different* arguments for each form
-        if extra_form_kwargs:
-            formset_class.form = staticmethod(curry(formset_class.form, **extra_form_kwargs))
-
-        return formset_class(**self.get_formset_kwargs())
+    can_delete = True
+    save_as_new = False
 
     def get_initial(self):
         """
@@ -209,103 +204,6 @@ class BaseFormSetMixin(object):
         Returns the form class to use with the formset in this view
         """
         return self.form_class
-
-    def get_formset(self):
-        """
-        Returns the formset class from the formset factory
-        """
-        return formset_factory(self.get_form_class(), **self.get_factory_kwargs())
-
-    def get_formset_kwargs(self):
-        """
-        Returns the keyword arguments for instantiating the formset.
-        """
-        kwargs = {}
-
-        # We have to check whether initial has been set rather than blindly passing it along,
-        # This is because Django 1.3 doesn't let inline formsets accept initial, and no versions
-        # of Django let generic inline formset handle initial data.
-        initial = self.get_initial()
-        if initial:
-            kwargs['initial'] = initial
-
-        if self.request.method in ('POST', 'PUT'):
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
-        return kwargs
-
-    def get_factory_kwargs(self):
-        """
-        Returns the keyword arguments for calling the formset factory
-        """
-        kwargs = {
-            'extra': self.extra,
-            'max_num': self.max_num,
-            'can_order': self.can_order,
-            'can_delete': self.can_delete,
-        }
-
-        if self.get_formset_class():
-            kwargs['formset'] = self.get_formset_class()
-
-        return kwargs
-
-
-class FormSetMixin(BaseFormSetMixin, ContextMixin):
-    """
-    A mixin that provides a way to show and handle a formset in a request.
-    """
-
-    def get_success_url(self):
-        """
-        Returns the supplied URL.
-        """
-        if self.success_url:
-            url = self.success_url
-        else:
-            # Default to returning to the same page
-            url = self.request.get_full_path()
-        return url
-
-    def formset_valid(self, formset):
-        """
-        If the formset is valid redirect to the supplied URL
-        """
-        return HttpResponseRedirect(self.get_success_url())
-
-    def formset_invalid(self, formset):
-        """
-        If the formset is invalid, re-render the context data with the
-        data-filled formset and errors.
-        """
-        return self.render_to_response(self.get_context_data(formset=formset))
-
-
-class BaseInlineFormSetMixin(BaseFormSetMixin):
-    """
-    Base class for constructing an inline formSet within a view
-
-    IMPORTANT: Because of a Django bug, initial data doesn't work here in Django 1.3
-    """
-    model = None
-    inline_model = None
-    fk_name = None
-    formset_class = BaseInlineFormSet
-    exclude = None
-    fields = None
-    formfield_callback = None
-    can_delete = True
-    save_as_new = False
-
-    def get_initial_data(self):
-        """
-        Returns the initial data to use for formsets on this view.
-
-        The value is an empty dict because we don't actually use this.
-        """
-        return {}
 
     def get_context_data(self, **kwargs):
         """
@@ -332,7 +230,21 @@ class BaseInlineFormSetMixin(BaseFormSetMixin):
         """
         Returns the keyword arguments for instantiating the formset.
         """
-        kwargs = super(BaseInlineFormSetMixin, self).get_formset_kwargs()
+        kwargs = {}
+
+        # We have to check whether initial has been set rather than blindly passing it along,
+        # This is because Django 1.3 doesn't let inline formsets accept initial, and no versions
+        # of Django let generic inline formset handle initial data.
+        initial = self.get_initial()
+        if initial:
+            kwargs['initial'] = initial
+
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+
         kwargs['save_as_new'] = self.save_as_new
         kwargs['instance'] = self.object
         return kwargs
@@ -341,13 +253,16 @@ class BaseInlineFormSetMixin(BaseFormSetMixin):
         """
         Returns the keyword arguments for calling the formset factory
         """
-        kwargs = super(BaseInlineFormSetMixin, self).get_factory_kwargs()
-        kwargs.update({
+        kwargs = {
+            'extra': self.extra,
+            'max_num': self.max_num,
+            'can_order': self.can_order,
+            'can_delete': self.can_delete,
             'exclude': self.exclude,
             'fields': self.fields,
             'formfield_callback': self.formfield_callback,
             'fk_name': self.fk_name,
-        })
+        }
         if self.get_form_class():
             kwargs['form'] = self.get_form_class()
         if self.get_formset_class():
@@ -360,16 +275,47 @@ class BaseInlineFormSetMixin(BaseFormSetMixin):
         """
         return inlineformset_factory(self.model, self.get_inline_model(), **self.get_factory_kwargs())
 
+    def construct_formset(self):
+        """
+        Returns an instance of the formset
+        """
+        formset_class = self.get_formset()
+        extra_form_kwargs = self.get_extra_form_kwargs()
 
-class InlineFormSetMixin(BaseInlineFormSetMixin, FormSetMixin, SingleObjectMixin):
+        # Hack to let as pass additional kwargs to each forms constructor. Be aware that this
+        # doesn't let us provide *different* arguments for each form
+        if extra_form_kwargs:
+            formset_class.form = staticmethod(curry(formset_class.form, **extra_form_kwargs))
+
+        return formset_class(**self.get_formset_kwargs())
+
+
+class InlineFormSetMixin(BaseInlineFormSetMixin, SingleObjectMixin, ContextMixin):
     """
     A mixin that provides a way to show and handle a inline formset in a request.
     """
 
+    def get_success_url(self):
+        """
+        Returns the supplied URL.
+        """
+        if self.success_url:
+            url = self.success_url
+        else:
+            # Default to returning to the same page
+            url = self.request.get_full_path()
+        return url
+
     def formset_valid(self, formset):
         self.object_list = formset.save()
-        return super(InlineFormSetMixin, self).formset_valid(formset)
+        return HttpResponseRedirect(self.get_success_url())
 
+    def formset_invalid(self, formset):
+        """
+        If the formset is invalid, re-render the context data with the
+        data-filled formset and errors.
+        """
+        return self.render_to_response(self.get_context_data(formset=formset))
 
 class BaseInlineFormSetView(InlineFormSetMixin, View):
     """
