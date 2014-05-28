eb294a4aac90d76076c367980795144aaa5c1b7c
Jeroen Dekkers <jeroen@dekkers.ch>
2014-05-28 23:55:30 +0200

    Add Python 3 trove classifier

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'Programming Language :: Python']
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3']
 )
