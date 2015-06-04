1ebf63b557b8b98d722a820404e127c238f6772e
Andrew Ingram <andy@andrewingram.net>
2015-06-04 11:11:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-extra-views',
-    version='0.6.5',
+    version='0.7.0',
     url='https://github.com/AndrewIngram/django-extra-views',
     install_requires=[
         'Django >=1.3',
