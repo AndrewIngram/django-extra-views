fd87e62e7e930941afabea2a58af6c9624811f63
Andrey Shevtsov <freeworlder@gmail.com>
2017-07-18 13:57:22 +0200

    Default module export for new messages mixins

--- extra_views/__init__.py
+++ extra_views/__init__.py
@@ -1,5 +1,5 @@
 from extra_views.formsets import FormSetView, ModelFormSetView, InlineFormSetView
-from extra_views.advanced import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSet, NamedFormsetsMixin
+from extra_views.advanced import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSet, NamedFormsetsMixin, FormSetSuccessMessageMixin, SuccessMessageMixin
 from extra_views.dates import CalendarMonthView
 from extra_views.contrib.mixins import SearchableListMixin, SortableListMixin
 
