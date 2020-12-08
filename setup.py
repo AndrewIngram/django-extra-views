3ab652d9e4c8391028162262fd8549bb77ffe48a
sdolemelipone <mark.gensler@protonmail.com>
2020-12-08 09:25:52 +0000

    Removed Python 2.7 from tox and travis

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
 )
