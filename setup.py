319527803ce872f0b5d552fdd1bb094e685176ee
sdolemelipone <mark.gensler@protonmail.com>
2020-12-08 10:17:25 +0000

    Removed Django 1.11, Added Python 3.8 and Django 3.1 to tox and travis

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="django-extra-views",
     version=VERSION,
     url="https://github.com/AndrewIngram/django-extra-views",
-    install_requires=["Django >=1.11"],
+    install_requires=["Django >=2.1"],
     description="Extra class-based views for Django",
     long_description=open("README.rst", "r").read(),
     license="MIT",
