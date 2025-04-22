93174e4f4aceb8c27780d29357d37e0158da8338
Mark Gensler <mark.gensler@protonmail.com>
2025-04-22 15:40:32 +0100

    Release 0.16.

--- extra_views/__init__.py
+++ extra_views/__init__.py
@@ -10,7 +10,7 @@ from extra_views.contrib.mixins import SearchableListMixin, SortableListMixin
 from extra_views.dates import CalendarMonthView
 from extra_views.formsets import FormSetView, InlineFormSetView, ModelFormSetView
 
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 __all__ = [
     "CreateWithInlinesView",
